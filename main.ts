d34cb8319a069e1d77ad36fc51e00b57135b657c
Svein-Tore <svenar89@gmail.com>
2020-08-10 12:57:49 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-basic.forever(function () {
-    for (let index = 0; index < 4; index++) {
-        AirBit.sendCppm(
-        0,
-        0,
-        0,
-        0,
-        0
-        )
-    }
-})
+//leave empty
